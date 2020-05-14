8650c3ffe4a80fea98ee2cb71d5c979867e80d7d
Eche <eche@mobnia.com>
2020-05-14 10:22:37 +0100

    remove base.php. Initialise with index.php

--- public/index.php
+++ public/index.php
@@ -10,14 +10,14 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-/*
- * Set autoload and make application root directory accessible
- */
-require_once '../base.php';
+require_once "vendor/autoload.php";
 
 $dotenv = Dotenv::createImmutable( dirname(__DIR__));
 $dotenv->load();
 
+/*
+ * Eloquent set up for application wide access
+ */
 $capsule = new Capsule();
 $capsule->addConnection([
     'driver'    => getenv("DB_DRIVER"),
